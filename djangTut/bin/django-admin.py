57763197722c468702dc91cb9d268d766dd6d931
madprestes <otavio.prestes@gmail.com>
2021-10-23 23:13:22 -0300

    My Django Girls app, first commit

---
+++ djangTut/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/otavioprestes/Documents/workspace/pyGirls/djangoTuts/djangTut/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
